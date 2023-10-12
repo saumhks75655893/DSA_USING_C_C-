544812c2fb402e7392d35c8fd1ab346a7f5abc1e
himanshu <himanshukumar75655893@gmail.com>
2023-10-12 11:48:59 +0530

    files added

---
+++ 01_ARRAY.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char c = 'a';
+    printf("%c\n", c);
+
+    int a[5] = {1,2,3,4,5}; 
+    for(int i=0; i<5; i++)
+    {
+        printf("%d", a[i]);
+        printf("\n");
+    }
+
+    return 0;
+}
