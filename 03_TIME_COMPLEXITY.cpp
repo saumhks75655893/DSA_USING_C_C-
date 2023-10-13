38fc477597e214c77eaba9366544e9775e98422b
himanshu <himanshukumar75655893@gmail.com>
2023-10-13 10:50:10 +0530

    03_TIME_COMPLEXITY.cpp added 2 files

---
+++ 03_TIME_COMPLEXITY.cpp
@@ -0,0 +1,32 @@
+// Find the sum of all number from range x to y.
+// Experimenal analysis : ------
+
+#include <iostream>
+using namespace std;
+
+int sum_in_range(int x, int y)
+{
+    //  Experimental analysis : ------
+    int result = 0;
+    for (int i=x; i<=y; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+int sum_in_range_optimized(int x, int y)
+{
+    // mathematical approach :------------
+
+    int n = (y - x + 1);
+    int a = x;
+    int result = (n * (2 * a + (n - 1) * 1)) / 2;
+    return result;
+}
+int main()
+{
+    cout << "Sum from x to y : " << sum_in_range(2, 10000000);
+    cout << "Sum from x to y : " << sum_in_range_optimized(2, 10000000);
+    return 0;
+}
