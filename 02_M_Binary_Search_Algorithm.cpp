8ae3dbe2a84f8b11c4dfdce20c14f7e2db188c9d
himanshu <himanshukumar75655893@gmail.com>
2023-10-17 11:53:04 +0530

    02_M_Binary_Search_Algorithm.cpp , in this file binary search algorithm implemented

---
+++ 02_M_Binary_Search_Algorithm.cpp
@@ -0,0 +1,82 @@
+//  --------------------  : BINARY SERACH ALGORITHM : --------------------------- :
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int search, n;
+    // Taking the number input from user
+    cout << "Enter the number of array's item : ";
+    cin >> n;
+
+    // initializing the array
+    int a[n];
+    //  taking input from the user for the array's elements.
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    // Printing the element of the array.
+    cout << "\nThe array is : \n"
+         << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << "th element is : " << a[i] << endl;
+    }
+
+    // Taking input of Searching element
+
+    cout << "\n\nEnter the element to search : ";
+    cin >> search;
+    int Beg = 1;
+    int End = n;
+
+    // Binary search algorith (non - recursive)
+    if (a[Beg] <= a[End])
+    {
+        while (Beg <= End)
+        {
+            int mid = int((Beg + End) / 2);
+            if (a[mid] == search)
+            {
+                cout << search << " found at the position : " << mid;
+                break;
+            }
+            else if (search < a[mid])
+            {
+                End = mid - 1;
+            }
+            else
+            {
+                Beg = mid + 1;
+            }
+        }
+    }
+    else if (a[Beg] >= a[End])
+    {
+        int Beg = 1;
+        int End = n;
+        while (Beg >= End)
+        {
+            int mid = int((Beg + End) / 2);
+            if (a[mid] == search)
+            {
+                cout << search << " found at the position : " << mid;
+                break;
+            }
+            else if (search > a[mid])
+            {
+                End = mid - 1;
+            }
+            else
+            {
+                Beg = mid + 1;
+            }
+        }
+    }
+    else
+    {
+        cout << search << " not found in the array. " << endl;
+    }
+}
