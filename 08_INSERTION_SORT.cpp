215515bb292594a467d43a451f8b0197863d622e
himanshu <himanshukumar75655893@gmail.com>
2023-10-20 11:42:59 +0530

    08_INSERTION_SORT.cpp modified and 09_QUICK_SORT.cpp a new file added

--- 08_INSERTION_SORT.cpp
+++ 08_INSERTION_SORT.cpp
@@ -1,32 +1,49 @@
 // ---------------------- : INSERTION SORT : -------------------------------- //
 
-#include<iostream>
-using namespace std; 
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    int n; 
-    cout<<"Enter the size of the array : "; 
-    cin>>n; 
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
 
-    //  inserting the element of the array : 
+    //  inserting the element of the array :
 
-    int a[n]; 
-    cout<<"Enter the value of the array : "; 
-    for(int i=1; i<=n; i++)
+    int a[n];
+    cout << "Enter the value of the array : ";
+    for (int i = 1; i <= n; i++)
     {
-        cin>>a[i]; 
+        cin >> a[i];
     }
 
-    // displaying the unsorted array : 
+    // displaying the unsorted array :
 
-    cout<<"\n\n The unsorted array is : "; 
-    for(int i=1; i<=n; i++)
+    cout << "\n\n The unsorted array is : ";
+    for (int i = 1; i <= n; i++)
     {
-        cout<<a[i]<<"   "; 
+        cout << a[i] << "   ";
     }
 
-    //  INSERTION SORT ALGORITHM : ---------------
+    //  INSERTION SORT ALGORITHM :
 
+    for (int j = 2; j <= n; j++)
+    {
+        int key = a[j];
+        int i = j - 1;
+        while (i > 0 && a[i] > key)
+        {
+            a[i + 1] = a[i];
+            i = i - 1;
+        }
+        a[i + 1] = key;
+    }
+    // displaying the unsorted array :
 
+    cout << "\n\n The unsorted array is : ";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << a[i] << "   ";
+    }
 }
