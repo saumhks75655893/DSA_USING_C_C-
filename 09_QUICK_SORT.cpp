bd9cf35d2fde35642ab7021fc1abc4e3dae98b6f
himanshu <himanshukumar75655893@gmail.com>
2023-10-21 11:07:19 +0530

    09_QUICK_SORT.CPP MODIFIED

--- 09_QUICK_SORT.cpp
+++ 09_QUICK_SORT.cpp
@@ -1,2 +1,51 @@
-//                                            QUICK SORT                                                
+//                                            QUICK SORT
 
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int first, int last)
+{
+    int pivot = arr[last];
+    int i = first - 1;
+    int j = first;
+
+    for (; j < last; i++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    // now i pointing to the last element
+    // correct position for pivot will be - i+1
+    swap(arr[i + 1], arr[last]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int first, int last)
+{
+    // base case
+    if (first >= last)
+    {
+        return;
+    }
+    // recursive case
+    int pi = partition(arr, first, last);
+    quickSort(arr, first, pi - 1);
+    quickSort(arr, pi + 1, last);
+}
+int main()
+{
+    int arr[] = {20, 12, 35, 16, 18, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "The array is : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "  , ";
+    }
+    cout << endl;
+}
