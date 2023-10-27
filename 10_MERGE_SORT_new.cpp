e9bb2a3e374ec561c69fb9cf0e581a0bc527b9dd
himanshu <himanshukumar75655893@gmail.com>
2023-10-27 12:25:28 +0530

    files 10_MERGE_SORT_new.cpp AND A NEW FILE ADDED 11_HEAP_SORT.cpp

--- 10_MERGE_SORT_new.cpp
+++ 10_MERGE_SORT_new.cpp
@@ -13,9 +13,58 @@ void printArray(int a[], int n)
     cout << "\n";
 }
 
-int mergeSort(int a[], int low, int mid, int high)
+//  merge function : -----------
+
+void Merge(int a[], int low, int mid, int high)
 {
-    ;
+    int i, j, k, b[high-low+1];
+    i = low;
+    j = mid + 1;
+    k = low;
+    while (i <= mid && j <= high)
+    {
+        if (a[i] < a[j])
+        {
+            b[k] = a[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            b[k] = a[j];
+            j++;
+            k++;
+        }
+    }
+    while (i <= mid)
+    {
+        b[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j <= high)
+    {
+        b[k] = a[j];
+        j++;
+        k++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        a[i] = b[i];
+    }
+}
+
+//  merge sort algorithm  : - - - - - - - - -- - - -  -- - -   -- - -  --  -- -- - - - -
+void mergeSort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid;
+        mid = int((low + high) / 2);
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+        Merge(a, low, mid, high);
+    }
 }
 //  main function : ---------
 int main()
@@ -39,6 +88,7 @@ int main()
 
     int low = 0;
     int high = n - 1;
-    int mid = (low + high) / 2;
-    mergeSort(a, low, mid, high);
+    mergeSort(a, low, high);
+    cout << "Sorted array is : ";
+    printArray(a, n);
 }
---
+++ 11_HEAP_SORT.cpp
@@ -0,0 +1,11 @@
+// -----------------------------------------  :   HEAP SORT  :  -------------------------------------------- // 
+/*
+//  procedure to perform heapsort : -----------
+
+1. construct a max-heap for the  given set of elements.
+2. exchange the first and the last element , reduce the size of the heap by 1 & construct again a max-heap for remaining (n-1)  elements.
+
+3. Repeat step 2 until only one element is leftout. 
+
+*/
+
